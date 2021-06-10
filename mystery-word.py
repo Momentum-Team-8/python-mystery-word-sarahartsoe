ba9b8e68012f021f9ffbbbf0417f6a3b1110acc4
sarahartsoe <sarahartsoe@gmail.com>
2021-06-10 18:57:25 -0400

    added letter input and validated for only 1 character

--- mystery-word.py
+++ mystery-word.py
@@ -4,3 +4,6 @@ import random
 with open("words.txt") as f:
     word_list = f.read().splitlines()
     print(f)
+    letter = input("What letter do you guess?")
+    if len(letter) >= 2:
+        print("Your guess can only be one letter!")
