e461a4504f266723cc9d5a05b8d9c0c2d6a8b7b7
sarahartsoe <sarahartsoe@gmail.com>
2021-06-11 13:54:49 -0400

    updated print statement for user instructions

--- mystery-word.py
+++ mystery-word.py
@@ -1,9 +1,39 @@
 import random
 
+# difficult easy = 1(4-5 letters), normal = 2(6-7 letters), hard = 3(8+letters)
+
+EASY_WORDS = [
+    'bang', 'seed', 'sell', 'inch', 'firm', 'meal', 'mess', 'rate', 'fill', 'goal', 'full', 'fade', 'herb', 'body', 'goat', 'disk', 'rich', 'silk', 'plot', 'gene', 'riot', 'zero', 'cast', 'dose', 'stun',
+    'throw', 'drift', 'tease', 'sweat', 'siege', 'irony', 'vague', 'spell', 'young', 'brain', 'color', 'spill', 'forge', 'glory', 'torch', 'slant', 'yearn', 'bacon', 'board', 'spine', 'width', 'enjoy', 'drama', 'wound',
+]
+
+NORM_WORDS = [
+    'stable', 'insure', 'sermon', 'candle', 'suntan', 'rabbit', 'origin', 'cancel', 'waiter', 'stress', 'attack', 'deadly', 'debate', 'reform', 'prayer', 'master', 'safety', 'castle', 'reward', 'linear', 'horror', 'tumble', 'export', 'matter', 'tumour',
+    'appoint', 'package', 'brother', 'pudding', 'premium', 'trainer', 'feeling', 'realize', 'balance', 'meaning', 'private', 'account', 'quarrel', 'dribble', 'abandon', 'overeat', 'rainbow', 'concede', 'command', 'silence', 'musical', 'railcar', 'warrant', 'peasant', 'opinion',
+]
+
+HARD_WORDS = [
+    'correction', 'conviction', 'monopoly', 'patience', 'uncertainty', 'acceptance', 'confusion', 'conception', 'analysis', 'pleasure', 'regulation', 'threshold', 'transmission', 'interface', 'notebook', 'priority', 'neighbour', 'horseshoe', 'vegetable', 'ambition', 'philosophy', 'crosswalk', 'judgment', 'execution', 'transaction', 'comfortable', 'commission', 'elaborate',
+    'parameter', 'identity', 'exclusive', 'understand', 'translate', 'chauvinist', 'withdrawl', 'evolution', 'domestic', 'catalogue', 'conversation', 'coalition', 'competence', 'freighter', 'cooperative', 'abundant', 'blackmail', 'isolation', 'communist', 'infinite', 'treasurer', 'governor',
+]
+
+print("Do you have what it takes to find the mystery word?")
+print("Choose a level- ")
+print("Easy is 4-5 letters")
+print("Normal is 6-7 letters")
+print("Hard is 8+ letters")
+print("You have 8 guesses - guess one letter per try.")
+print("Game over when you guess the word or run out of tries!")
+print("Good luck and DON'T fuck it up!")
 
 with open("words.txt") as f:
     word_list = f.read().splitlines()
     print(f)
-    letter = input("What letter do you guess?")
+    number = input("Choose your level. Enter 1 for Easy, 2 for Normal, or 3 for Hard: ")
+    if number == 1:
+        wordChoice = random.choice(EASY_WORDS)
+    
+    letter = input("Good choice! What letter do you guess? ")
     if len(letter) >= 2:
         print("Your guess can only be one letter!")
+
